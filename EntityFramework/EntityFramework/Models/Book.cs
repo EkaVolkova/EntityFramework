06f712f0ca8ba768c0bd6429a19fb5c0c0d27561
Ekaterina Volkova <koweevaea@gmail.com>
2024-02-20 20:27:18 +0500

    Добавила в User список полученных на руки книг, добавила в Book пользователя, взявшего книгу на руки

--- EntityFramework/EntityFramework/Models/Book.cs
+++ EntityFramework/EntityFramework/Models/Book.cs
@@ -2,8 +2,24 @@
 {
     public class Book
     {
+        /// <summary>
+        /// Id книги (первичный ключ)
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Название книги
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Год выпуска книги
+        /// </summary>
         public uint PublishYear { get; set; }
+
+        /// <summary>
+        /// Пользователь, на руках у которого находится книга
+        /// </summary>
+        public User User { get; set; }
     }
 }
--- EntityFramework/EntityFramework/Models/User.cs
+++ EntityFramework/EntityFramework/Models/User.cs
@@ -2,8 +2,24 @@
 {
     public class User
     {
+        /// <summary>
+        /// Id пользователя (первичный ключ)
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// E-mail пользователя
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Список книг, находящихся на руках у пользователя
+        /// </summary>
+        public List<Book> Books { get; set; }
     }
 }
